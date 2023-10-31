8003ed1b704e97e16d5027d41ada546c39476341
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-10-31 15:46:27 -0400

    code working in script.js for prompt and making an array of order. work in progess: object function.

--- style.js
+++ style.js
@@ -1,5 +1,3 @@
-alert("Hello!");
-
 /*
 A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors. 
 They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. 
@@ -8,22 +6,43 @@ In this case, they will be able to observe that they have ordered three vanilla,
 */
 
 //prompt user
-  //Welcome to Frodo's Froyo Festival
+  //Welcome to Frodo's Froyo Festival🍦
   // "Enter a list of comma-separated froyo flavors"
     //test with: vanilla,vanilla,vanilla,strawberry,coffee,coffee
   //accept user input as a string called customerOrderString
 
+  const customerOrderString = prompt(`Welcome to Frodo's Froyo Festival🍦\nEnter a list of comma-separated froyo flavors: `)
+
   //alert --- "thanks for your order, open the console and look at the results"
-  //create an array called customerOrderArray via split ", " method,
+  alert("Thanks for your order, open the console and look at the results");
+
+  //create an array called customerOrderArray via split "," method,
+  const customerOrderArray = customerOrderString.split(',');
+  
+  //initialize customerOrderObject as an empty object
+  const customerOrderObject = {};
+  console.log(customerOrderObject);
 
   //make this a function called froyoOrderTaker
-  //initialize customerOrderObject as an empty object
-  //loop through array and add to an object called customerOrderObject
-  //logic to add in if key is not in there. if key is in add 1 to value
+  const froyoOrderMaker = (froyoFlavors) => {
+    //loop through array and add to an object called customerOrderObject
+      //loop through array and add to an object called customerOrderObject
+      //logic to add in if key is not in there. if key is in add 1 to value
         //no vanilla in object ---> add froyoObject[vanilla] = 1
         //yes vanilla in keys ----> froyoObject[vanilla] +=1 
+    for (let i = 0; i < froyoFlavors.length; i++) {
+      if (froyoFlavors[i] in customerOrderObject) {
+        customerOrderObject[i]++;
+      } else {
+        customerOrderObject[i] = 1;
+      }
+    }
+  }
 
-  //console out the froyoOrderTaker      
+//console out the froyoOrderTaker   
+console.log(customerOrderObject);
+
+   
 
 
 ////or//////////
